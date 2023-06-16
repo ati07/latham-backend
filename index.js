25089d55a706745ed8c018f5f3878cee12343cd3
ati07 <atiurrahman.ansari@gmail.com>
2023-06-16 11:20:20 +0530

    testing

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ cron.schedule('59 23 * * *', () => {
   // console.log('running a task every minute');
   updateChargebackStatus()
 });
-cron.schedule('45 23 * * *',()=>{
-  // console.log('running a task every minute verfiy');
+cron.schedule('* * * * *',()=>{
+  // console.log('running a task every minute verfiy'45 23);
   verifyScript()
 })
 dotenv.config();
